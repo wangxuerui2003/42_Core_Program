1c88a91fe50d8f064745cbf55b2d57b1bd663567
Wang Xuerui <wangxuerui2003@gmail.com>
2022-08-09 19:44:31 +0800

    libft, gnl, ft_printf

---
+++ libft/Libft/ft_strchr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wxuerui <wxuerui@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 16:36:51 by wxuerui           #+#    #+#             */
+/*   Updated: 2022/07/26 16:58:39 by wxuerui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (c > 256)
+		c -= 256;
+	if (s == NULL)
+		return (NULL);
+	if (c == 0)
+	{
+		while (s[i])
+			i++;
+		return ((char *)s + i);
+	}
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((char *)s + i);
+		i++;
+	}
+	return (NULL);
+}
